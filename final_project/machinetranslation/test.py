90de5fccef589378e052fcfac03b02e58a72da0f
LuiiV <lu.vasquez1991@gmail.com>
2023-02-23 16:29:57 -0500

    Final changes

--- final_project/machinetranslation/test.py
+++ final_project/machinetranslation/test.py
@@ -1,18 +1,23 @@
 import unittest
 from translator import french_to_english, english_to_french
 
-class testEnglish(unittest.TestCase):
+class TestEnglish(unittest.TestCase):
 
     def test_english_to_french(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+    def test_not_english_to_french(self):
         self.assertNotEqual(english_to_french('Hello'), 'Hello')
 
 
-class testFrench(unittest.TestCase):
+class TestFrench(unittest.TestCase):
 
     def test_french_to_english(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+    def test_not_french_to_english(self):
         self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour')
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,4 +1,3 @@
-import json
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -13,13 +12,20 @@ def english_to_french(english_text):
 
     authenticator = IAMAuthenticator(apikey)
 
-    language_translator = LanguageTranslatorV3(version = '2018-05-01', authenticator = authenticator)
+    language_translator = LanguageTranslatorV3(
+        version = '2023-01-13', 
+        authenticator=authenticator
+        )
 
     language_translator.set_service_url(url)
 
-    french_translation = language_translator.translate(text = english_text, model_id = 'en-fr').get_result()
+    french_translation = language_translator.translate(
+        text = english_text, 
+        model_id = 'en-fr')
 
-    french_text = french_translation.get['translations'][0].get['translation']
+    dictionary = french_translation.get_result()
+
+    french_text = dictionary['translations'][0]['translation']
 
     return french_text
 
@@ -27,12 +33,19 @@ def french_to_english(french_text):
 
     authenticator = IAMAuthenticator(apikey)
 
-    language_translator = LanguageTranslatorV3(version = '2018-05-01', authenticator = authenticator)
+    language_translator = LanguageTranslatorV3(
+        version = '2023-01-13', 
+        authenticator=authenticator
+        )
 
     language_translator.set_service_url(url)
 
-    english_translation = language_translator.translate(text = french_text, model_id = 'fr-en').get_result()
+    english_translation = language_translator.translate(
+        text = french_text, 
+        model_id = 'fr-en')
 
-    english_text = english_translation.get['translations'][0].get['translation']
+    dictionary = english_translation.get_result()
+
+    english_text = dictionary['translations'][0]['translation']
 
     return english_text
